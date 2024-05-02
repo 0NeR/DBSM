8b7b44a7c83361e18bd965950a6f2e6c66b6375a
0NeR <ner8650@gmail.com>
2024-05-02 17:56:18 +0300

    переделано

--- UserLog/UserLog.py
+++ UserLog/UserLog.py
@@ -1,46 +1,42 @@
-import sys
-from datetime import datetime
 import csv
 import sqlite3
+import sys
+from datetime import datetime
 
 
-def main(log_fname, users_fname):
+def main(log_file, users_file):
     log_data = []
-    with open(log_fname, encoding='koi8_r') as fin:
-        for row in csv.reader(fin):
-            cur_row = [i.encode('koi8_r').decode('utf-8') for i in row]
-            if cur_row[0] != "#error":
-                cur_row[0] = cur_row[0].split("_")[-1]
-                cur_row[1] = cur_row[1][1:]
-                if datetime.strptime(cur_row[1], '%Y-%m-%d %H:%M:%S'):
-                    if cur_row[1][13] != ":":
-                        cur_row[1] = cur_row[1][:11] + '0' + cur_row[1][11:]
-                    log_data.append(cur_row)
-    log_data.sort()
-    
     users_data = []
-    with open(users_fname, encoding='koi8_r') as fin:
-        
-        for row in csv.reader(fin):
-
-            cur_row = row[0].split("\t")
-            if (cur_row[0][:5] == "User_" and cur_row[0][5:].isdigit()):
-                cur_row[0] = cur_row[0][5:]
-                users_data.append(cur_row)
+    with open(log_file, encoding='koi8_r') as fin:
+        for i in csv.reader(fin):
+            j = []
+            for k in i:
+                 j.append(k.encode('koi8_r').decode('utf-8'))
+            if j[0] != "#error":
+                j[0] = j[0].split("_")[-1]
+                j[1] = j[1][1:]
+                if datetime.strptime(j[1], '%Y-%m-%d %H:%M:%S'):
+                    if j[1][13] != ":":
+                        j[1] = j[1][:11] + '0' + j[1][11:]
+                    log_data.append(j)
+    with open(users_file, encoding='koi8_r') as fin:
+        for i in csv.reader(fin):
+            j = i[0].split("\t")
+            if j[0][:5] == "User_":
+                if j[0][5:].isdigit():
+                    j[0] = j[0][5:]
+                    users_data.append(j)                
+    log_data.sort()
     users_data.sort()
-    
-    
     with sqlite3.connect('users_log.s3db') as conn:
         cur = conn.cursor()
-
         for user_id, time, bet, win in log_data:
-        	cur.execute('INSERT INTO LOG (user_id, time, bet, win) VALUES (?, ?, ?, ?)',(user_id, time, bet, win))
-
+          cur.execute('INSERT INTO LOG (user_id, time, bet, win) VALUES (?, ?, ?, ?)',(user_id, time, bet, win))       
         for user_id, email, geo in users_data:
-        	cur.execute('INSERT INTO USERS (user_id, email, geo) VALUES ((SELECT user_id FROM LOG WHERE user_id = ?), ?, ?)',(user_id, email, geo))
+          cur.execute('INSERT INTO USERS (user_id, email, geo) VALUES ((SELECT user_id FROM LOG WHERE user_id = ?), ?, ?)',(user_id, email, geo))  
         conn.commit()
 
 
 
-if __name__ =='__main__':
-	main(sys.argv[1], sys.argv[2])
+if name =='main':
+  main(sys.argv[1], sys.argv[2])
